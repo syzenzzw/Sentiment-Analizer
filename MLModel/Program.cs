11d7c167e2a788406610dbec0c8b85ef7eae5417
syzenzzw <oisyzenx@gmail.com>
2025-02-07 12:32:47 -0300

    first commit

---
+++ MLModel/Program.cs
@@ -0,0 +1,12 @@
+﻿using MLModel;
+
+var sampleData = new SentimentModel.ModelInput()
+{
+    Col0 = @"Crust is not good.",
+};
+
+var result = SentimentModel.Predict(sampleData);
+
+var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+
+Console.WriteLine("Text: " + sampleData.Col0 + ", sentiment: " + sentiment);
